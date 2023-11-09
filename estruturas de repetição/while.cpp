541d382424630fb7b7d5add0de91b930b5d7ce19
magnofarias <144641484+magnofarias@users.noreply.github.com>
2023-11-08 22:01:14 -0300

    update while.cpp

--- estruturas de repetição/while.cpp
+++ estruturas de repetição/while.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int x = 0;
+int main()
+{
+    while (x<=0){
+        cout<<"digite o x novamente: ";
+        cin>>x;    
+    }
+    
+    return 0;
+}
